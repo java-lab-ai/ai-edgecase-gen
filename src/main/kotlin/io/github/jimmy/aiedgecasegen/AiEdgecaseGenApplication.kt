3aef87c07b756bce34e16e544a6adf3171f1dd62
jimmy <Jimmy.YanJm@outlook.com>
2025-06-15 16:58:11 +0800

    init: ai-edgecase-gen project with Gradle & basic structure

---
+++ src/main/kotlin/io/github/jimmy/aiedgecasegen/AiEdgecaseGenApplication.kt
@@ -0,0 +1,11 @@
+package io.github.jimmy.aiedgecasegen
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiEdgecaseGenApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiEdgecaseGenApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/jimmy/aiedgecasegen/AiEdgecaseGenApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.jimmy.aiedgecasegen
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiEdgecaseGenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
